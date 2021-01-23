63b38042f3415a8c0215084b56b2af4f3e7ccfd9
catherinewu12 <wucatherine12@gmail.com>
2021-01-22 23:42:22 -0500

    Kingston located on map

--- project.js
+++ project.js
@@ -3,10 +3,9 @@
 let map, infoWindow;
 
 function initMap() {
-    console.log("hello");
     map = new google.maps.Map(document.getElementById("map"), {
-      center: { lat: -34.397, lng: 150.644 },
-      zoom: 6,
+      center: { lat: 44.2312, lng: -76.486 },
+      zoom: 15,
     });
     infoWindow = new google.maps.InfoWindow();
     const locationButton = document.createElement("button");
